ae5601a35f0f24685d5f252c995d24912eae3f1b
Boruiqiao <39862938+qiaoborui@users.noreply.github.com>
2020-04-11 11:08:49 +0800

    Update pixiv_search.js

--- script/pixiv_search.js
+++ script/pixiv_search.js
@@ -1,5 +1,5 @@
 let url = 'https://pixiv.mlyx.workers.dev/?api=pixiv'
-$task.fetch({ url: "https://raw.githubusercontent.com/qiaoborui/Quanx_conf/master/script/slivertocoin.js" }).then(response => {
+$task.fetch({ url: url }).then(response => {
 if (error){
 $done({});
 }
