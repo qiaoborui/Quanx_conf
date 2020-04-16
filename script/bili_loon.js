7e949939d2833bcadb277e8ac832760719bb61ff
Boruiqiao <39862938+qiaoborui@users.noreply.github.com>
2020-04-17 01:53:21 +0800

    Create bili_loon.js

---
+++ script/bili_loon.js
@@ -0,0 +1,19 @@
+var url = $request.url
+var acckey = url.match(/access_key=(.*?)&/)[1]
+$persistentStore.write(acckey, "tk")
+console.log(acckey)
+
+let body = $response.body
+body=JSON.parse(body)
+
+var tab = body['data']['tab']
+var top = body['data']['top']
+var bottom = body['data']['bottom']
+bottom.splice(2,2)
+top.splice(0,1)
+delete tab[1].default_selected
+tab[3]["default_selected"]=1
+tab.splice(5,3)
+//console.log(tab)
+body=JSON.stringify(body)
+$done({body}) 
