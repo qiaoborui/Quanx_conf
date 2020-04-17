a980bf48087481b08330c044ca7593d816253abd
Boruiqiao <qiaoborui@users.noreply.github.com>
2020-04-17 11:44:36 +0800

    Create slivertocoin.js

---
+++ script/Loon_script/slivertocoin.js
@@ -0,0 +1,12 @@
+var ak = $persistentStore.read("tk");
+var url = "https://api.live.bilibili.com/pay/v1/Exchange/silver2coin?access_key="+ak;
+var headers = {"Content-Type": "application/x-www-form-urlencoded","User-Agent": "bili-universal/9290 CFNetwork/1121.2.2 Darwin/19.3.0 os/ios model/iPhone 7 mobi_app/iphone osVer/13.3.1 network/1",};
+var myRequest = {
+    url: url,
+    headers:headers,
+    method: method, // Optional, default GET.
+};
+$httpClient.post(myRequest,function(response,data){
+    var msg = JSON.parse(data).msg;
+    $notification.post("银瓜子换硬币📺",msg,"")
+});
